b4970a5cf74168a1b19dafc5f85db1c55c28acc1
Christian Walde <walde.christian@googlemail.com>
2010-10-03 22:47:28 +0200

    make NoXS display useful data under ActivePerl when failing
    For some reason Carp won't work under ActivePerl and just throw "unknown error" instead of detailing what was attempted to load with a stacktrace. Due to this, we replace it with a normal die, accompanied by a homebrew stacktrace and remove the dependency on Carp.

--- t/lib/MakeMaker/Test/NoXS.pm
+++ t/lib/MakeMaker/Test/NoXS.pm
@@ -2,8 +2,6 @@ package MakeMaker::Test::NoXS;
 
 # Disable all XS loading.
 
-use Carp;
-
 require DynaLoader;
 require XSLoader;
 
@@ -13,7 +11,32 @@ delete $DynaLoader::{boot_DynaLoader};
 # This isn't 100%.  Things like Win32.pm will crap out rather than
 # just not load.  See ExtUtils::MM->_is_win95 for an example
 no warnings 'redefine';
-*DynaLoader::bootstrap = sub { confess "Tried to load XS for @_"; };
-*XSLoader::load        = sub { confess "Tried to load XS for @_"; };
+*DynaLoader::bootstrap = sub { die "Tried to load XS for @_\nTrace:\n"._stacktrace()."\n"; };
+*XSLoader::load        = sub { die "Tried to load XS for @_\nTrace:\n"._stacktrace()."\n"; };
+
+sub _stacktrace {
+    my $call_string = '';
+
+    ## first, get raw stack info as deep as we can
+    my @calls;
+    my $i = 0;
+    while ( my @call = caller($i) ) {
+        push @calls, \@call;
+        $i++;
+    }
+
+    shift @calls;
+
+    my @call_strings;
+    for ( reverse @calls ) {
+        next if !$_->[2];
+        $_->[1] = 'main' if $_->[0] eq 'main';
+        push @call_strings, "   $_->[3]() called at $_->[1] line $_->[2]\n";
+    }
+
+    $call_string = join '', @call_strings;
+
+	return $call_string;
+}
 
 1;
